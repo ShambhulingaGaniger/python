edee6e0fbcea8353b01a6a5473e20747c97e1e69
unknown <shambhulingaganager@gmail.com>
2024-07-03 10:02:16 +0530

    this is my frst commit

---
+++ com.py
@@ -0,0 +1,8 @@
+x=complex(input("enter the first complex number "))
+y=complex(input("enter  the second complex  number"))
+print(type(x)," ",type(y))
+print("addition",x+y)
+print("sub",x-y)
+print("floral div",x/y)
+print("mul",x*y)
+print("power",x**y)
